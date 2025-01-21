e22029c39ab45b78b1871bcb363100b4689d3d4d
vivek <nitheeshvivek2@gmail.com>
2025-01-21 20:26:02 +0530

    initial setup finished

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mlops
 This is a test project to learn mlops
+
+
+Execute the following commands 
+
+conda create -n <env_name> python=3.11 -y
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MLPipeline", 
+    ver = "0.0.0",
+    author="vivek", 
+    author_email="nitheeshvivek2@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
